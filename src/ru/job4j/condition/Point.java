d42f9c1afebf56432215fb6fcd8106f8e8868b0e
max-piter <61771015+max-piter@users.noreply.github.com>
2021-07-08 11:12:25 +0300

    9.1. String [#237407]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -7,7 +7,7 @@ public class Point {
 
     public static void main(String[] args) {
         double result = Point.distance(0, 0, 2, 0);
-        double result2 = Point.distance(2,3,6,-4);
+        double result2 = Point.distance(2, 3, 6, -4);
         System.out.println("result (0, 0) to (2, 0) "  + result);
     }
 }
