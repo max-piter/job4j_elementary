6b28641ba0d6c73d5683b2f65b3e8b63ffe5564e
max-piter <61771015+max-piter@users.noreply.github.com>
2021-07-10 21:41:24 +0300

    5.9. Крест в псевдографике. [#177182]

---
+++ src/ru/job4j/loop/Slash.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+public class Slash {
+    public static void draw(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int c = 1; c <= size; c++) {
+                if (i == c) {
+                    System.out.print("0");
+                } else if (i + c  == size + 1) {
+                    System.out.print("0");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Draw by 3");
+        draw(3);
+        System.out.println("Draw by 5");
+        draw(5);
+    }
+}
