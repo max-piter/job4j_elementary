6b3fdb94be649ce2d475391db31e8d6da87840e9
max-piter <61771015+max-piter@users.noreply.github.com>
2021-07-10 22:22:34 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива surnames равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Max Korovkin";
+        names[1] = "Andrew Korovkin";
+        names[2] = "Kirill Korovkin";
+        names[3] = "Alex Korovkin";
+        for (int i = 0; i < 4; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
