53b0bda8953db0334f5b8545d409b72352466131
max-piter <61771015+max-piter@users.noreply.github.com>
2021-07-11 14:02:57 +0300

    5.5. Простое число [#171694 #176132]

---
+++ src/ru/job4j/array/Check.java
@@ -0,0 +1,10 @@
+package ru.job4j.array;
+
+public class Check {
+    public static boolean mono(boolean[] data) {
+        boolean result = true;
+        /*        for () { */
+        /*        }        */
+        return result;
+    }
+}
