e656b5db9943f90a0ca406702af9fe4246a50320
max-piter <61771015+max-piter@users.noreply.github.com>
2021-07-12 12:35:31 +0300

    6.4. Слово начинается с ... [#41585 #176698]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -4,9 +4,7 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
         for (int i = 0; i < pref.length; i++) {
-            if (pref[i] == word[i]) {
-                result = true;
-            } else {
+            if (pref[i] != word[i]) {
                 result = false;
                 break;
             }
